7fc78385b48a0f233114ef0a5c6e1033c8858793
jasonZhang <www.fully@qq.com>
2017-02-21 17:42:21 +0800

    add controller

---
+++ src/main/java/com/jason/controller/PscFileController.java
@@ -0,0 +1,12 @@
+package com.jason.controller;
+
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Created by jason on 2017/2/21.
+ */
+public class PscFileController {
+    public ModelAndView listFile(){
+        return null;
+    }
+}
